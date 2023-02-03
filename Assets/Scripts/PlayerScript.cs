6bd102d818d3c91a8852f1fed34b197da1022b41
Teerapat Prompunjai <theerapat.nick@gmail.com>
2023-02-03 20:33:19 +0700

    walking working now

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -8,13 +8,18 @@ public class PlayerScript : MonoBehaviour
 
     private Animator _animator;
     private PlayerInput _playerInput;
+    private SpriteRenderer _renderer;
 
     private bool isWalk;
+
+    private Vector2 walkDirection;
+    public Vector2 WalkSpeed;
 
     private void Awake()
     {
         _animator = GetComponent<Animator>();
         _playerInput = GetComponent<PlayerInput>();
+        _renderer = GetComponent<SpriteRenderer>();
     }
 
     // // Start is called before the first frame update
@@ -24,22 +29,32 @@ public class PlayerScript : MonoBehaviour
     // }
 
     // Update is called once per frame
-    // void Update()
-    // {
-
-    // }
+    void Update()
+    {
+        transform.Translate( walkDirection.x * WalkSpeed.x * Time.deltaTime, 0, walkDirection.y * WalkSpeed.y * Time.deltaTime );
+    }
 
     public void walk( InputAction.CallbackContext context )
     {
-        Debug.Log( context );
+        Debug.Log( context.ReadValue<Vector2>() );
         Debug.Log( "walk" );
         if ( context.performed ){
             Debug.Log( "yes walk" );
             _animator.SetBool( "walk", true );
+
         }
 
         if ( context.canceled ){
              _animator.SetBool( "walk", false );
         }
+
+        walkDirection = context.ReadValue<Vector2>();
+        if( walkDirection.x < 0 ){
+            _renderer.flipX = true;
+        }
+        if( walkDirection.x > 0 ){
+            _renderer.flipX = false;
+        }
+
     }
 }
